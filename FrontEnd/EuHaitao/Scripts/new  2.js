5351039f10516a3c99f3b7a0bab972ebacd10846
li m <senothe164@gmail.com>
2014-03-25 18:00:19 +0100

    Creation

---
+++ FrontEnd/EuHaitao/Scripts/new  2.js
@@ -0,0 +1,396 @@
+$(document).ready(function () {
+var a='<div class="banners">
+<div class="banner-frame">
+<div id="banner-1">
+	<a class="banner-link" href="#">
+		<img class="banner-images" src="Images/banner1.jpg" />
+	</a>
+</div>
+
+<div class="nav-float">
+	<ul class="nav-float-list">
+		<li class="nav-float-item"><a href="" class="nav-float-a">热门优惠</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">母婴玩具</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">化妆美容</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">精品服饰</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">鞋包配饰</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">居家用品</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">运动休闲</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">保健用品</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">影音游戏</a></li>
+		<li class="nav-float-item"><a href="" class="nav-float-a">电子产品</a></li>
+	</ul>
+</div>
+
+	</div>
+</div>
+
+
+<div class="records">
+	<div class="records-frame">
+<div class="map">
+	<img src="Images/map.gif" />
+</div>
+
+<div class="records-list">
+<div class="record">
+2014.01.15 10:25 北京 7KG 已签收 此次包裹投递共用7天 德国到北京平均需时8天 最快6天 最慢12天
+</div>
+<div class="record">
+2014.01.15 11:35 武汉 10KG 已签收 此次包裹投递共用8天 德国到北京平均需时9天 最快6天 最慢12天
+</div>
+<div class="record">
+2014.01.15 15:15 上海 7KG 已签收 此次包裹投递共用7天 德国到北京平均需时8天 最快6天 最慢12天
+</div>
+<div class="record">
+2014.01.15 17:35 武汉 10KG 已签收 此次包裹投递共用7天 德国到北京平均需时9天 最快6天 最慢12天
+</div>
+<div class="record-more">
+	<a href="">
+		更多城市信息...
+	</a>
+</div>
+</div>
+	</div>
+</div>
+
+
+<div class="recommendation-marks">
+<!-- 注意此处宽度 超过1000将导致换行 -->
+	<div class="recommendation-marks-frame">
+		<div class="recommendation-mark">
+			<a href="">
+				<img src="Images/mark3.gif"/>
+			</a>
+		</div>
+		<div class="recommendation-mark">
+			<a href="">
+				<img src="Images/mark2.gif"/>
+			</a>
+		</div>
+		<div class="recommendation-mark">
+			<a href="">
+				<img src="Images/mark1.gif"/>
+			</a>
+		</div>
+		<div class="recommendation-mark">
+			<a href="">
+				<img src="Images/mark4.gif"/>
+			</a>
+		</div>
+		<div class="recommendation-mark">
+			<a href="">
+				<img src="Images/mark2.gif"/>
+			</a>
+		</div>
+		<div class="recommendation-mark">
+			<a href="">
+				<img src="Images/mark1.gif"/>
+			</a>
+		</div>
+
+	</div>
+	
+	<div class="recommendation-mark-more">
+			<a href="">
+				其他推荐
+			</a>
+	</div>
+</div>
+
+<div class="recommendation-products">
+	<div class="recommendation-product">
+		
+		<div class="product">
+			<a href="">
+				<img src="Images/product1.gif" />
+				<div>150元/盒 已成交156笔</div>
+			</a>
+		</div>
+		<div class="product">
+			<a href="">
+				<img src="Images/product2.gif" />
+				<div>230元/盒 已成交126笔</div>
+			</a>
+		</div>
+		<div class="product">
+			<a href="">
+				<img src="Images/product3.gif" />
+				<div>1880元 已成交103笔</div>
+			</a>
+		</div>
+		<div class="product">
+			<a href="">
+				<img src="Images/product4.gif" />
+				<div>1780元 已成交57笔</div>
+			</a>
+		</div>
+		<div class="product_">
+			<a href="">
+				<img src="Images/product5.gif" />
+				<div>1500元 已成交16笔</div>
+			</a>
+		</div>
+		
+	</div>
+</div>
+
+<div class="recommendation-list">
+	<div class="recommendation-list-frame">
+		<div class="items1">
+			<div class="item-title1">
+				<a href="">
+					Top10
+				</a>	
+			</div>
+			<div class="item1">
+				<a href="">
+					<img class="item-img" src="Images/item1.gif" />
+					<div class="item-info">
+						德国喜宝2段婴儿奶粉
+						<br />
+						150元/盒 已成交156笔
+					</div>
+				</a>	
+			</div>
+			<div class="item1">
+				<a href="">
+					<img class="item-img" src="Images/item2.gif" />
+					<div class="item-info">
+						德国femibion叶酸
+						<br />
+						230元/盒 已成交126笔
+					</div>
+				</a>	
+			</div>
+			<div class="item1">
+				<a href="">
+					<img class="item-img" src="Images/item3.gif" />
+					<div class="item-info">
+						德国Fissler高压锅
+						<br />
+						1880元 已成交103笔
+					</div>
+				</a>	
+			</div>
+			<div class="item1">
+				<a href="">
+					<img class="item-img" src="Images/item4.gif" />
+					<div class="item-info">
+						德国博朗电动牙刷高压锅
+						<br />
+						1780元 已成交57笔
+					</div>
+				</a>	
+			</div>
+			
+			<div class="item1">
+				<a href="">
+					<img class="item-img" src="Images/item5.gif" />
+					<div class="item-info">
+						德国Cybex儿童汽车座椅
+						<br />
+						1500元 已成交16笔
+					</div>
+				</a>	
+			</div>
+		
+
+		</div>
+		
+		<div class="items2">
+			<div class="item-title2">
+				<a class="item-title2-a" href="">
+					SALE%
+				</a>
+			</div>
+			<div class="item2">
+				<a href="">
+					<img class="item-img" src="Images/item1.gif" />
+					<div class="item-info">
+						德国喜宝2段婴儿奶粉
+						<br />
+						150元/盒 已成交156笔
+					</div>
+				</a>	
+			</div>
+			<div class="item2">
+				<a href="">
+					<img class="item-img" src="Images/item2.gif" />
+					<div class="item-info">
+						德国femibion叶酸
+						<br />
+						230元/盒 已成交126笔
+					</div>
+				</a>	
+			</div>
+			<div class="item2">
+				<a href="">
+					<img class="item-img" src="Images/item3.gif" />
+					<div class="item-info">
+						德国Fissler高压锅
+						<br />
+						1880元 已成交103笔
+					</div>
+				</a>	
+			</div>
+			<div class="item2">
+				<a href="">
+					<img class="item-img" src="Images/item4.gif" />
+					<div class="item-info">
+						德国博朗电动牙刷高压锅
+						<br />
+						1780元 已成交57笔
+					</div>
+				</a>	
+			</div>
+			
+			<div class="item2">
+				<a href="">
+					<img class="item-img" src="Images/item5.gif" />
+					<div class="item-info">
+						德国Cybex儿童汽车座椅
+						<br />
+						1500元 已成交16笔
+					</div>
+		    	</a>	
+		    </div>
+
+		</div>
+		
+		<div class="items3">
+			<div class="item-title3">
+				<a href="">
+					聚德优品
+				</a>
+			</div>
+			
+			<div class="item3">
+				<a href="">
+					<img class="item-img" src="Images/item1.gif" />
+					<div class="item-info">
+						德国喜宝2段婴儿奶粉
+						<br />
+						150元/盒 已成交156笔
+					</div>
+				</a>	
+			</div>
+			<div class="item3">
+				<a href="">
+					<img class="item-img" src="Images/item2.gif" />
+					<div class="item-info">
+						德国femibion叶酸
+						<br />
+						230元/盒 已成交126笔
+					</div>
+				</a>	
+			</div>
+			<div class="item3">
+				<a href="">
+					<img class="item-img" src="Images/item3.gif" />
+					<div class="item-info">
+						德国Fissler高压锅
+						<br />
+						1880元 已成交103笔
+					</div>
+				</a>	
+			</div>
+			<div class="item3">
+				<a href="">
+					<img class="item-img" src="Images/item4.gif" />
+					<div class="item-info">
+						德国博朗电动牙刷高压锅
+						<br />
+						1780元 已成交57笔
+					</div>
+				</a>	
+			</div>
+			
+			<div class="item3">
+				<a href="">
+					<img class="item-img" src="Images/item5.gif" />
+					<div class="item-info">
+						德国Cybex儿童汽车座椅
+						<br />
+						1500元 已成交16笔
+		    		</div>
+		    	</a>	
+               </div>
+		</div>
+	</div>
+</div>
+
+
+<!-- 以下为部件的简单堆叠， 尚需其他代码将其整合-->
+<div class="brick">
+	<a href="" class="brick-a">注册或登录</a>
+</div>
+
+<div class="brickk">
+	<a href="">
+		<div class="brick-select">
+		</div>
+	</a>
+	
+	<a href="">
+		<div class="brick-selected">
+		</div>
+	</a>
+	
+	<a href="">
+		<div class="brick-b">
+		MTK的易优
+		</div>
+	</a>
+
+</div>
+
+<div class="brickkk">
+	<a href="">
+		<div class="brick-selected">
+		</div>
+	</a>
+	
+	<a href="">
+		<div class="brick-select">
+		</div>
+	</a>
+	<a href="">
+		<div class="brick-b">
+		MTK的易优
+		</div>
+	</a>
+	
+	<div class="chariot">
+		<img src="Images/chariot.png" />
+	</div>
+	
+	<div class="progress">
+		<img class="box" src="Images/box1.png" />
+		<img class="progress-bar" src="Images/progress23.png" />
+	</div>
+	
+	<div class="progress">
+		<img class="box" src="Images/box2.png" />
+		<img class="progress-bar" src="Images/progress23.png" />
+	</div>
+	
+	<div class="progress">
+		<img class="box" src="Images/box3.png" />
+		<img class="progress-bar" src="Images/progress23.png" />
+	</div>
+	
+	<div class="call">
+		<img src="Images/call.png" />
+	</div>
+	
+	<div class="info">
+		<img src="Images/info.png" />
+	</div>
+</div>
+<div class="help">
+</div>';
+	   
+	   document.get
+	   
+	   
---
+++ FrontEnd/EuHaitao/Scripts/slide.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+	var div = $("div.content");
+
+    $("#nav1").click(function () {
+        div.animate({ left: '+=1000px' }, "slow");
+		setTimeout('window.location.href="Guide.html"',1000);
+    });
+
+	
+    $("#nav2").click(function () {
+		var height = $("div.content").height();
+		div.animate({ top: '-=' + height }, "slow");
+		setTimeout('window.location.href="Home.html"',1000);
+    });
+
+    $("#nav3").click(function () {
+        div.animate({ left: '-=1000px' }, "slow");
+		setTimeout('window.location.href="Register.html"',1000);
+    });
+
+
+});
